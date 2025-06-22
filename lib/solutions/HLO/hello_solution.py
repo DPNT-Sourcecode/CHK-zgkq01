dd0b0b56e383a9317b378fe68f327899cf5aaa1e
sbx_user1051 <sbx_user1051@169.254.12.221>
2025-06-22 12:03:32 +0000

    Sun Jun 22 12:03:32 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,4 +6,7 @@ class HelloSolution:
         if not isinstance(friend_name, str):
             raise ValueError(f" Friend Name : {friend_name} is not a string")
 
-        return ("Hello, World!")
+        if friend_name:
+            return f"Hello {friend_name}!"
+        else:
+            return "No friend name given!"

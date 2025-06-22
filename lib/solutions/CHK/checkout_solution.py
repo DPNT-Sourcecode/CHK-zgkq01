aa5491ac8078024251ef26babb9409626729c66a
sbx_user1051 <sbx_user1051@169.254.12.221>
2025-06-22 12:45:22 +0000

    Sun Jun 22 12:45:22 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,14 +19,10 @@ class CheckoutSolution:
             'A': 50,
             'B': 30,
             'C': 20,
-            'D': 15
+            'D': 15,
+            'E': 40
             }
-        
-        special_offer_dict = {
-            'A': {'count': 3, 'price': 130},
-            'B': {'count': 2, 'price': 45},
-            }
-        
+            
         sku_count = {}
         for sku in skus:
             if sku not in price_dict: # illegal characters that are not items 
@@ -35,18 +31,50 @@ class CheckoutSolution:
         
         total_price = 0
 
-        # For loop goign through and adding to total price based on special offers and items in the sku
-        for sku, count in sku_count.items(): 
-            if sku in special_offer_dict:
-                offer = special_offer_dict[sku]
-                offer_sets = count // offer['count']
-                remaining = count % offer["count"]
-                total_price += offer_sets * offer['price']
-                total_price += remaining * price_dict[sku]
-            else:
-                total_price += count * price_dict[sku]
+        # Fidning best offer based on basket, 5A for 200 is better than 3A for 130 therefore we handle 5A offers on priority
+        if 'A' in sku_count:
+            a_count = sku_count['A']
+            offer_5a = a_count // 5
+            rem_a = a_count % 5
+            offer_3a = rem_a // 3
+            final_rem_a = rem_a % 3
+
+            total_price += offer_5a * 200
+            total_price += offer_3a * 130
+            total_price += final_rem_a * 50
+
+        # E offers, if 2 Es are bought we add to the free B
+        count_free_B = 0
+        if 'E' in sku_count:
+            e_count = sku_count['E']
+            count_free_B = e_count // 2
+
+            total_price += e_count * 40
+
+        #B Handling with offers and with the E offer for free Bs
+        if 'B' in sku_count:
+            b_count = sku_count['B']
+            end_b_count = max(0, b_count - count_free_B) # Handling by removing free B from the E offer
+
+            offer_2b = end_b_count // 2
+            rem_b = end_b_count % 2
+
+            total_price += offer_2b * 45
+            total_price += rem_b * 30
+
+
+        if 'C' in sku_count:
+            c_count = sku_count['C']
+            
+            total_price += c_count * 20
+
+        if 'D' in sku_count:
+            d_count = sku_count['D']
+            
+            total_price += d_count * 15
+            
+        
+
         
         return total_price
             
-
-
